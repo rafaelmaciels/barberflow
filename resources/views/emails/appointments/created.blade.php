84e8d84a402342b57d531bbe942555a8758108e2
Rafael Maciel <rafael@barberflow.com>
2026-07-22 23:35:48 -0300

    chore: limpeza geral de arquivos temporarios e obsoletos

--- resources/views/emails/appointments/created.blade.php
+++ resources/views/emails/appointments/created.blade.php
@@ -1,7 +1,6 @@
 <x-mail::message>
-# Confirmação de Agendamento Olá **{{ $data['cliente_nome'] ?? 'Cliente' }}**, Seu agendamento foi recebido com sucesso na nossa barbearia! **Detalhes:**
+# Confirmação de Agendamento Olá **{{ $data['cliente_nome'] ?? 'Cliente' }}**, agendou atendimento com sucesso na nossa barbearia! **Detalhes:**
 - **Data:** {{ date('d/m/Y', strtotime($data['data'])) }}
-- **Horário:** {{ $data['hora'] }} Aguardamos você no horário marcado! Obrigado,<br>
-Equipe BarberFlow
+- **Horário:** {{ $data['hora'] }} 
 </x-mail::message>
 
